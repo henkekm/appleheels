5b5a1798c0cde35d665d7de60618a6c5fe1eeadf
Samantha Morco <morkotiks@gmail.com>
2016-08-12 11:24:12 -0700

    down method complete

--- app/assets/javascripts/TerminalMenuTwo.js
+++ app/assets/javascripts/TerminalMenuTwo.js
@@ -23,15 +23,14 @@ Appleheels.TerminalMenuTwo.prototype = {
   },
 
   getDownAssignment: function (num) {
-    console.log("down", Appleheels.downAssigment[num]);
     if (num == Appleheels.downAssigment.length) {
       num = 0
     } else if (num == -1) {
-      num = Appleheels.downAssigment.length
+      num = (Appleheels.downAssigment.length - 1)
     };
     this.game.downCount = num;
+    console.log(num);
     return Appleheels.downAssigment[num];
-    console.log("down count", this.game.downCount);
   },
 
   initialDown: function () {
@@ -51,12 +50,12 @@ Appleheels.TerminalMenuTwo.prototype = {
     this.attrDownAssignment = [];
 
     // INIT Down Button attribute
-    this.downAssignmentText = this.add.bitmapText (0, 164, 'rollingThunder', 'Jump Power', 16);
+    this.downAssignmentText = this.add.bitmapText (0, 164, 'rollingThunder', 'Down Assignment', 16);
     this.downAssignmentText.x = this.margin.left;
     this.downAssignmentText.tint = this.terminalText.color;
     this.attrDownAssignment.push(this.downAssignmentText);
     this.downAssignmentDisplay = this.add.bitmapText (0, 164, 'rollingThunder', this.initialDown(), 16);
-    this.downAssignmentDisplay.x = 512 - (this.downAssignmentDisplay.textWidth + this.margin.right);
+    this.downAssignmentDisplay.x = 500 - (this.downAssignmentDisplay.textWidth + this.margin.right);
     this.downAssignmentDisplay.tint = this.terminalText.color;
     this.attrDownAssignment.push(this.downAssignmentDisplay);
 
@@ -85,31 +84,26 @@ Appleheels.TerminalMenuTwo.prototype = {
     // Press RIGHT to increment value
     if (this.cursor.right.isDown) {
       this.getDownAssignment(this.game.downCount + 1);
-
       this.downAssignmentDisplay.text = this.getDownAssignment(this.game.downCount);
-
-      this.downAssignmentDisplay.x = 512 - (this.downAssignmentDisplay.textWidth + this.margin.right);
+      this.downAssignmentDisplay.x = 500 - (this.downAssignmentDisplay.textWidth + this.margin.right);
       this.decDownAssignment.x = this.downAssignmentDisplay.x - this.margin.left;
     };
     // Press LEFT to decrement value
     if (this.cursor.left.isDown) {
-      // this.setDownAssignment (this.getDownAssignment() - 1);
-      // this.downAssignmentDisplay.text = String(this.getDownAssignment());
       this.getDownAssignment(this.game.downCount - 1);
 
-      this.downAssignmentDisplay.x = 512 - (this.downAssignmentDisplay.textWidth + this.margin.right);
+      this.downAssignmentDisplay.x = 500 - (this.downAssignmentDisplay.textWidth + this.margin.right);
       this.decDownAssignment.x = this.downAssignmentText.x - this.margin.left;
     };
     // Press ESC to exit
     if (this.backButton.isDown) {
       console.log("EXIT");
-      console.log("DownAssignment", this.getDownAssignment(this.game.downCount));
       this.game.downMethod = this.getDownAssignment(this.game.downCount);
       console.log("DownAssignment", this.getDownAssignment(this.game.downMethod));
       $.ajax({
         url: "/game/" + this.game.gameId,
         type: "PUT",
-        data: {game_instance: { jump_power: this.game.downMethod } },
+        data: {game_instance: { down_method: this.game.downMethod } },
       });
       this.state.start('Game');
     };
