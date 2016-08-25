26edb8cac8b596dfea0bfe8eeff297cc641d19be
Kevin Henke <henkekm@gmail.com>
2016-08-25 01:10:06 -0400

    Places terminalThree
    Refactors terminalThree interaction to only toggle, rather than cycle
    through array.

--- app/assets/javascripts/TerminalMenuThree.js
+++ app/assets/javascripts/TerminalMenuThree.js
@@ -3,9 +3,6 @@ Appleheels.TerminalMenuThree = function(game) {
 
   this.cursor;
   this.backButton;
-  this.attributes;
-  this.attrFavNumber;
-  this.attrJumpPower;
 
 };
 
@@ -22,15 +19,12 @@ Appleheels.TerminalMenuThree.prototype = {
     color: 0xE81717
   },
 
-  getObjective: function (num) {
-    if (num == Appleheels.objectives.length) {
-      num = 0
-    } else if (num == -1) {
-      num = (Appleheels.objectives.length - 1)
-    };
-    this.game.objectiveCount = num;
-    console.log(num);
-    return Appleheels.objectives[num];
+  getObjective: function (_currentVal) {
+    if (_currentVal == true) {
+      return Appleheels.objectives[1];
+    } else {
+      return Appleheels.objectives[0];
+    }
   },
 
   initialObjective: function () {
@@ -46,29 +40,20 @@ Appleheels.TerminalMenuThree.prototype = {
     this.titleText.x = 256 - (this.titleText.textWidth / 2);
     this.titleText.tint = this.terminalText.color;
 
-    this.attributes = [];
-    this.attrObjectiveAssignment = [];
-
     // INIT Objective attribute
-    this.objectiveAssignmentText = this.add.bitmapText (0, 164, 'rollingThunder', 'Down Assignment', 16);
+    this.objectiveAssignmentText = this.add.bitmapText (0, 164, 'rollingThunder', 'Objective', 16);
     this.objectiveAssignmentText.x = this.margin.left;
     this.objectiveAssignmentText.tint = this.terminalText.color;
-    this.attrObjectiveAssignment.push(this.objectiveAssignmentText);
     this.objectiveAssignmentDisplay = this.add.bitmapText (0, 164, 'rollingThunder', this.initialObjective(), 16);
     this.objectiveAssignmentDisplay.x = 500 - (this.objectiveAssignmentDisplay.textWidth + this.margin.right);
     this.objectiveAssignmentDisplay.tint = this.terminalText.color;
-    this.attrObjectiveAssignment.push(this.objectiveAssignmentDisplay);
 
     this.decObjectiveAssignment = this.add.bitmapText (0, 164, 'rollingThunder', '-', 16);
     this.decObjectiveAssignment.x = this.objectiveAssignmentDisplay.x - this.margin.left;
     this.decObjectiveAssignment.tint = this.terminalText.color;
-    this.attrObjectiveAssignment.push(this.decObjectiveAssignment);
     this.incObjectiveAssignment = this.add.bitmapText (492, 164, 'rollingThunder', '+', 16);
     this.incObjectiveAssignment.tint = this.terminalText.color;
-    this.attrObjectiveAssignment.push(this.incObjectiveAssignment);
 
-    this.attributes.push(this.attrObjectiveAssignment);
-    // console.log(this.attributes[0]);
   },
 
   create: function () {
@@ -77,34 +62,28 @@ Appleheels.TerminalMenuThree.prototype = {
     this.cursor = this.input.keyboard.createCursorKeys();
     this.backButton = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
 
-    this.game.add.existing (this.titleText);
+    this.game.add.existing(this.titleText);
   },
 
   update: function () {
-    // Press RIGHT to increment value
-    if (this.cursor.right.isDown) {
-      this.getObjective(this.game.objectiveCount + 1);
-      this.objectiveAssignmentDisplay.text = this.getObjective(this.game.objectiveCount);
+    // Press RIGHT or LEFT to toggle death objective
+    if (this.cursor.right.isDown || this.cursor.left.isDown) {
+      this.game.isDie = !this.game.isDie;
+      this.objectiveAssignmentDisplay.text = this.getObjective(this.game.isDie);
       this.objectiveAssignmentDisplay.x = 500 - (this.objectiveAssignmentDisplay.textWidth + this.margin.right);
       this.decObjectiveAssignment.x = this.objectiveAssignmentDisplay.x - this.margin.left;
-    };
-    // Press LEFT to decrement value
-    if (this.cursor.left.isDown) {
-      this.getObjective(this.game.objectiveCount - 1);
-
-      this.objectiveAssignmentDisplay.x = 500 - (this.objectiveAssignmentDisplay.textWidth + this.margin.right);
-      this.decObjectiveAssignment.x = this.objectiveAssignmentText.x - this.margin.left;
     };
     // Press ESC to exit
     if (this.backButton.isDown) {
       console.log("EXIT");
-      this.game.objective = this.getObjective(this.game.objectiveCount);
-      console.log("ObjectiveAssignment", this.getObjective(this.game.objective));
+      this.game.objective = this.getObjective(this.game.isDie);
+
       $.ajax({
         url: "/game/" + this.game.gameId,
         type: "PUT",
         data: {game_instance: { objectives: this.game.objective } },
       });
+
       this.state.start('Game');
     };
 
